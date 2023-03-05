c21f0dbef04ab54c61d2eb4433037afa14ef388e
ibraheemdurrani <durranik106@gmail.com>
2023-03-05 06:43:53 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+const swiper = new Swiper('.swiper', {
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
+  });
+
+
+
+  /*show and hide nav bar */
+
+  const menuBtn = document.querySelector('#menu-btn');
+  const closeBtn = document.querySelector('#close-btn');
+  const menu = document.querySelector('nav .container ul');
+
+  menuBtn.addEventListener('click', () => {
+    menu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'inline-block';
+  })
+
+  closeBtn.addEventListener('click', () => {
+    menu.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+    closeBtn.style.display = 'none';
+  })
+
+    /*show and hide nav bar */
+
+
+    // add active class to clicked nav item  
+    const navItems = menu.querySelectorAll('li');
+
+    const changeActiveItem = () => {
+      navItems.forEach(item => {
+        const link = item.querySelector('a');
+        link.classList.remove('active');
+      })
+    }
+
+    navItems.forEach(item => {
+      const link = item.querySelector('a');
+      link.addEventListener('click', () => {
+        changeActiveItem();
+        link.classList.add('active');
+      })
+    })
+
+    // read more about 
+    const readMoreBtn =  document.querySelector('.read-more');
+    const readMoreContent = document.querySelector('.read-more-content');
+
+    readMoreBtn.addEventListener('click', () => {
+      readMoreContent.classList.toggle('show-content');
+      if(readMoreContent.classList.contains('show-content')){
+        readMoreBtn.textContent = 'Show less';
+      }
+      else{
+        readMoreBtn.textContent = 'Show more';
+      }
+    })
+
+
+    // show/hide skills items
+ const skillItems = document.querySelectorAll('section.skills .skill');
+
+ skillItems.forEach(skill => {
+  skill.querySelector('.head').addEventListener('click', () => {
+    skill.querySelector('.items').classList.toggle('show-items');
+  })
+ })
+
+
+//  add box shadow on scroll 
+window.addEventListener('scroll', () => {
+  document.querySelector('nav').classList.toggle('show-box-shadow',window.scrollY > 0)
+})
